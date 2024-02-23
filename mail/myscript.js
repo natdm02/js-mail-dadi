3903d8be49073187e0a6800dc3d474ca162b415e
natdm02 <natalidimarino2002>
2024-02-23 16:29:25 +0100

    [add]mail

--- mail/myscript.js
+++ mail/myscript.js
@@ -1,2 +1,23 @@
 // creo una lista di mail
 let email = ["mariorossi@gmail.com", "marcoverdi@icloud.com", "mariabianchi@libero.it", "ciroesposito@gmail.com", "federicapia@gmail.com"];
+
+// un bottone per il controllo
+var element = document.getElementById("btnEmail");
+element.addEventListener("click",
+    function(){
+        var emailUser = document.getElementById("inputEmail").value;
+        console.log(emailUser);
+
+//scritta "puoi accedere"
+            if (email.includes(emailUser)){
+                console.log("true");
+                document.getElementById("result").innerHTML ="Email accettata!";
+//  "non puoi accedere"
+                
+            } else { 
+                console.log("false");
+                document.getElementById("result").innerHTML = "Email rifiutata!";
+            }
+    }
+);
+
